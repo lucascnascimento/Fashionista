aedd6301e0293d738f4954b074ea75c34199cf62
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-06 15:05:14 -0300

    Criação da estrutura do backdrop

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { BackdropScreen } from "./styles";
+
+function Backdrop() {
+  return <BackdropScreen />;
+}
+
+export default Backdrop;
---
+++ src/components/Backdrop/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BackdropScreen = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 100;
+`;
