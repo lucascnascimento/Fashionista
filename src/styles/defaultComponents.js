21a031c25f80e3839621d668c91607265cf83b83
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-07 18:29:51 -0300

    Refatoração: Criação de componentes default para encapsular a imagem e a tag de desconto

---
+++ src/styles/defaultComponents.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const DefaultImgWrapper = styled.div`
+  display: flex;
+  position: relative;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export const DefaultDiscountTag = styled.div`
+  position: absolute;
+  background: black;
+  color: white;
+`;
