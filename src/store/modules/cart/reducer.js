c6de75a5a8d0054b5dba61445f7c60574331f639
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-07 22:28:17 -0300

    Refatoração: adição e remoção de item no carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,9 +1,40 @@
 import { ADD_ITEM, REMOVE_ITEM } from "../constants";
 
-const initialState = { items: [] };
+function cart(state = [], action) {
+  switch (action.type) {
+    case ADD_ITEM: {
+      const itemIndex = state.findIndex(
+        (product) =>
+          product.name === action.item.name && product.size === action.item.size
+      );
 
-function cart(state = initialState, action) {
-  return state;
+      if (itemIndex > -1) {
+        return state.map((product) => {
+          if (
+            product.name === action.item.name &&
+            product.size === action.item.size
+          ) {
+            return { ...product, amount: product.amount + 1 };
+          }
+          return product;
+        });
+      }
+
+      return [...state, action.item];
+    }
+
+    case REMOVE_ITEM: {
+      const itemIndex = state.findIndex(
+        (product) =>
+          product.name === action.item.name && product.size === action.item.size
+      );
+
+      return state.splice(itemIndex, 1);
+    }
+
+    default:
+      return state;
+  }
 }
 
 export default cart;
