3c13c1d7a8f3eab41a4f3c4c08be4d0551c5fc24
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-09 13:44:19 -0300

    Hero image usando um img como elemento

---
+++ src/components/HeroImage/HeroImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import image1 from "../../assets/images/adult-beautiful-elegant-eyewear-291762 (1).jpg";
+import image2 from "../../assets/images/irene-kredenets-pSVYyO-XlJk-unsplash (1).jpg";
+import image3 from "../../assets/images/tamara-bellis-LJqRUWr9V0w-unsplash (1).jpg";
+
+import { Hero } from "./styles";
+
+function HeroImage() {
+  return <Hero src={image2} />;
+}
+
+export default HeroImage;
---
+++ src/components/HeroImage/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Hero = styled.img`
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+`;
