dd9e59e3cc70a65471f3324bc3cf4844b07ebef0
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-12 22:50:24 -0300

    Refatoração: criação de constantes de estilo e cor

--- src/styles/stylesConsts.js
+++ src/styles/stylesConsts.js
@@ -14,14 +14,47 @@ export const sizes = {
 };
 
 export const device = {
-  medium: `(min-width: ${sizes.medium})`,
-  large: `(min-width: ${sizes.large})`,
+  biggerThanMedium: `(min-width: ${sizes.medium})`,
+  biggerThanLarge: `(min-width: ${sizes.large})`,
+  biggerThanTablet: `(min-width: ${sizes.tablet})`,
 };
 
-export const colors = {
-  bgGray: "f8f8f8",
+export const grayColors = {
+  bgGray: "#f8f8f8",
   lineGray: "#ccc",
   mediumGray: "#888",
   darkGray: "#444",
   veryDarkGray: "#222",
+};
+
+export const Colors = {
+  color1: "#EDC7b7",
+  color2: "#eee2dc",
+  color3: "#bab2b5",
+  color4: "#123c69",
+  color5: "#ac3b61",
+};
+
+export const SDRB = {
+  color1: "#2e1114",
+  color2: "#501b1d",
+  color3: "#64485c",
+  color4: "#83677b",
+  color5: "#AdADAD",
+};
+
+export const EYA = {
+  color1: "#EDC7b7",
+  color2: "#eee2dc",
+  color3: "#bab2b5",
+  color4: "#123c69",
+  color5: "#ac3b61",
+};
+
+export const MYW = {
+  color1: "#eae7dc",
+  color2: "#d8c3a5",
+  color3: "#8e8d8a",
+  color4: "#e98074",
+  color5: "#e85a4f",
 };
