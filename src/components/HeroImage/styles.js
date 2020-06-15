51f17848404cd7c429d9568883160fd5725ae994
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-15 10:38:14 -0300

    Refatoração: limpeza de arquivos desnecessários.Style: Posicionamento da imagem de fundo

--- src/components/HeroImage/styles.js
+++ src/components/HeroImage/styles.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
-export const Hero = styled.img`
+export const Hero = styled.div`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url(${(props) => props.img});
+
+  /* Set a specific height */
   height: 100vh;
   width: 100%;
-  object-fit: cover;
+
+  /* Position and center the image to scale nicely on all screens */
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
 `;
