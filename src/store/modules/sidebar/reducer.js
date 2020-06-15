5a1c7ed1f594f21612ec09b8f6c3fa3e6280e7c4
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-15 10:54:40 -0300

    Refatoração: limpeza do console.log()

--- src/store/modules/sidebar/reducer.js
+++ src/store/modules/sidebar/reducer.js
@@ -5,11 +5,9 @@ const initialState = { component: null };
 function sidebar(state = initialState, action) {
   switch (action.type) {
     case OPEN_SIDEBAR:
-      console.log(action);
       return { ...state, component: action.component };
 
     case CLOSE_SIDEBAR:
-      console.log(action);
       return { ...state, component: action.component };
 
     default:
