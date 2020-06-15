3661f78a4932cf7db77ccc88a3a53c789c50a56c
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-15 10:43:01 -0300

    Refatoração: O componente de loading agora aparece no carregamento do state

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import App from "./App";
+import Loading from "./components/Loading";
 
 import { store, persistor } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
